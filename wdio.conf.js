0c8b245419e3030c8cd58974f033c5887fc0679d
Pritam Bal <pritam.bal@cloudkaptan.com>
2026-02-11 21:18:02 +0530

    added 123

--- wdio.conf.js
+++ wdio.conf.js
@@ -29,7 +29,7 @@ export const config = {
     
        // specs: ['./test1/**/*.js'],
 
-        specs: ['./test_framework/salesforce.test1.ts'],
+        specs: ['./test_framework/salesforce.test.ts'],
 
     
     // Patterns to exclude.
